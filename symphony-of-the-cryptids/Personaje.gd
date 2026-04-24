0fcd6742e210d0a087c1c061be9dedf822dd1ef3
LucasBruno2022 <111883074+LucasBruno2022@users.noreply.github.com>
2026-04-24 20:19:03 -0300

    Primer movimiento del personaje
    Se añadió un scripts y luego atreves de los inputs se mueve en horizontal y vertical

---
+++ symphony-of-the-cryptids/Personaje.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@export var speed: int = 10
+var direction = Vector2(0,0)
+
+func _physics_process(_delta):
+	
+
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
+
+	velocity = direction * speed
+	move_and_slide()
