eb55b70031a7a4f43ecca3183ac96eec7347a3e3
LucasBruno2022 <111883074+LucasBruno2022@users.noreply.github.com>
2026-05-11 23:00:30 -0300

    Función de areas

--- symphony-of-the-cryptids/scripts/personaje.gd
+++ symphony-of-the-cryptids/scripts/personaje.gd
@@ -31,3 +31,22 @@ func DetectarInput():
 func enviar_input(nota):
 	if criptido_actual != null:
 		criptido_actual.siguiente_nota(nota)
+
+
+func _on_area_2d_area_entered(area):
+	var collider = area.get_parent()
+	if collider is Criptido:
+		criptido_actual = collider
+		print("Entraste en rango de un Criptido")
+
+		var secuencia = criptido_actual.obtener_secuencia()
+		print("Secuencia:", secuencia)
+
+	pass # Replace with function body.
+
+
+func _on_area_2d_area_exited(area):
+	if area.get_parent() == criptido_actual:
+		print("Saliste del rango del Criptido")
+		criptido_actual = null
+	pass # Replace with function body.
