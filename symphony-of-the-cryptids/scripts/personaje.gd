e33294667ba32ec773f9a40c7832a7baaa6cdef8
LucasBruno2022 <111883074+LucasBruno2022@users.noreply.github.com>
2026-05-11 22:57:35 -0300

    Funcion de enviar notas a criptido

--- symphony-of-the-cryptids/scripts/personaje.gd
+++ symphony-of-the-cryptids/scripts/personaje.gd
@@ -2,6 +2,9 @@ class_name Jugador
 extends CharacterBody2D
 
 @export var Velocidad: int = 50
+var Direccion
+var InputFlechas = []
+var criptido_actual = null
 
 func _physics_process(_delta):
 	
@@ -11,3 +14,20 @@ func _physics_process(_delta):
 	
 	
 	move_and_slide()
+
+func DetectarInput():
+	if Input.is_action_just_pressed("ui_up"):
+		enviar_input("up")
+	
+	if Input.is_action_just_pressed("ui_down"):
+		enviar_input("down")
+	
+	if Input.is_action_just_pressed("ui_left"):
+		enviar_input("left")
+	
+	if Input.is_action_just_pressed("ui_right"):
+		enviar_input("right")
+
+func enviar_input(nota):
+	if criptido_actual != null:
+		criptido_actual.siguiente_nota(nota)
