16413b73e26f8ec5d597a9ea82d354865332df2e
LucasBruno2022 <111883074+LucasBruno2022@users.noreply.github.com>
2026-06-02 20:22:57 -0300

    unos arreglos del TextureRect
    Se añadió un Contenedor a la escena para controlar los texture rect

--- symphony-of-the-cryptids/scripts/barra.gd
+++ symphony-of-the-cryptids/scripts/barra.gd
@@ -1,2 +1,7 @@
 class_name UIBarra
 extends Node2D
+
+@onready var flecha_D_Arriba = $flechaArriba
+@onready var flecha_D_Abajo = $flechaAbajo
+@onready var flecha_D_Izquierda = $flechaIzquierda
+@onready var flecha_D_Derecha = $flechaDerecha
