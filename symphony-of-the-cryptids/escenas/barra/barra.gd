b315aad0239ae042c234e6d7eadeb219147f5480
LucasBruno2022 <111883074+LucasBruno2022@users.noreply.github.com>
2026-06-02 22:51:16 -0300

    Se hicieron muchos cambios
    Se hicieron muchos cambios al momento de testear la barra, mucho no funcionaba porque no se lograba rotar ningún texture

---
+++ symphony-of-the-cryptids/escenas/barra/barra.gd
@@ -0,0 +1,10 @@
+class_name UiBarra
+extends Control
+
+@onready var flecha_D_interaccion = $flechaInput
+
+
+
+func _ready():
+	flecha_D_interaccion.rotation_degrees = 270
+	
--- symphony-of-the-cryptids/scripts/barra.gd
+++
@@ -1,7 +0,0 @@
-class_name UIBarra
-extends Node2D
-
-@onready var flecha_D_Arriba = $flechaArriba
-@onready var flecha_D_Abajo = $flechaAbajo
-@onready var flecha_D_Izquierda = $flechaIzquierda
-@onready var flecha_D_Derecha = $flechaDerecha
