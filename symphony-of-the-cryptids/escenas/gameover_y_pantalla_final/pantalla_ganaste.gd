0a56853f46cec19b6c18aad39f5c56459ef71217
MauroEscalante-Hub <ms.fangamesclasicos@gmail.com>
2026-06-10 03:09:16 -0300

    Todas las pantallas funcionales
    Todo funciona como debe ser, game over y juego completado

--- symphony-of-the-cryptids/escenas/gameover_y_pantalla_final/pantalla_ganaste.gd
+++ symphony-of-the-cryptids/escenas/gameover_y_pantalla_final/pantalla_ganaste.gd
@@ -1,17 +1,6 @@
 extends CanvasLayer
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	
-	pass
-
+@onready var GuitaIU = $VBoxContainer2/Puntuacion
 
 func _on_volver_al_menu_button_down() -> void:
 	get_tree().change_scene_to_file("res://escenas/menu_principal/menu_principal.tscn")
--- symphony-of-the-cryptids/scripts/jaula_de_criptido.gd
+++ symphony-of-the-cryptids/scripts/jaula_de_criptido.gd
@@ -3,22 +3,29 @@ extends Area2D
 
 @onready var Animation_player = $AnimationPlayer
 @onready var Centro = $CentrodeLaJaula
+@onready var Jaula_abierta = $JaulaAbierta 
+@onready var Jaula_cerrada = $JaulaCerrada
 @export var Criptido_actual: Criptido
 @export var Criptidos_Enjaulados: int = 0
 @export var Cantidad_De_Criptidos: int = 1
 var Dinero_Generado: int = 0
-var Dinero_Suficiente: int = 200
+var Dinero_Suficiente: int = 600
 var Cuota_cumplida: bool = false
 
-const Fade = preload("res://escenas/nivel/escenario.gd")
-var script_jaula = Fade.new()
+#const Fade = preload("res://escenas/nivel/escenario.gd")
+#var script_jaula = Fade.new()
+#Aca intente usar una funcion de otra escena pero no estoy
+#seguro de como funciona, no tiro error ni nada pero no funciono
 
 func _ready():
 	Criptido_actual = null
 
-#func _process(delta: float) -> void:
-#	Plata_Generada()
- 
+func _process(delta: float) -> void:
+	if Criptidos_Enjaulados > 0:
+		Jaula_abierta.visible = false
+		Jaula_cerrada.visible = true
+	
+
 func Plata_Generada():
 	if Dinero_Generado > Dinero_Suficiente:
 		Cuota_cumplida = true
