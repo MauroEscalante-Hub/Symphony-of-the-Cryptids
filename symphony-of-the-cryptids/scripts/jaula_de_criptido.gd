108249dd3a083ba7614405db5aa8f50198dd84e5
MauroEscalante-Hub <ms.fangamesclasicos@gmail.com>
2026-06-10 01:31:32 -0300

    Cambios_De_Escenrio
    Puli bastante el como funciona el script para completar el nivel, faltan unos detalles pero funciona

--- symphony-of-the-cryptids/scripts/jaula_de_criptido.gd
+++ symphony-of-the-cryptids/scripts/jaula_de_criptido.gd
@@ -1,14 +1,27 @@
 class_name Jaula
 extends Area2D
 
+@onready var Animation_player = $AnimationPlayer
 @onready var Centro = $CentrodeLaJaula
 @export var Criptido_actual: Criptido
 @export var Criptidos_Enjaulados: int = 0
 @export var Cantidad_De_Criptidos: int = 1
+var Dinero_Generado: int = 0
+var Dinero_Suficiente: int = 200
+var Cuota_cumplida: bool = false
+
+const Fade = preload("res://escenas/nivel/escenario.gd")
+var script_jaula = Fade.new()
 
 func _ready():
 	Criptido_actual = null
 
+#func _process(delta: float) -> void:
+#	Plata_Generada()
+ 
+func Plata_Generada():
+	if Dinero_Generado > Dinero_Suficiente:
+		Cuota_cumplida = true
 
 func _on_body_entered(body):
 	if body is Criptido:
@@ -16,6 +29,11 @@ func _on_body_entered(body):
 		print("El criptido ingreso a la jaula")
 		body.Estado_enjaulado()
 		Criptidos_Enjaulados =+ 1
-		if Criptidos_Enjaulados == Cantidad_De_Criptidos:
-			get_tree().change_scene_to_file("res://escenas/gameover_y_pantalla_final/game_over.tscn")
+		Dinero_Generado =+ 250
+		Plata_Generada()
+		#print(Dinero_Generado)
+		if Criptidos_Enjaulados == Cantidad_De_Criptidos and Cuota_cumplida == true:
+			#script_jaula.Fadeout
+			get_tree().change_scene_to_file("res://escenas/gameover_y_pantalla_final/pantalla_ganaste.tscn")
+			
 	
