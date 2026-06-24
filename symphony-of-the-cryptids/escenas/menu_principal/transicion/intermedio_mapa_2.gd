5a936ac0f9d6e417e0541fb23565270d49af3b77
MauroEscalante-Hub <ms.fangamesclasicos@gmail.com>
2026-06-24 17:06:35 -0300

    Sonidos_y_escenas_de_transicion
    Termine de agregar las escenas de transicion para los niveles, meti sonidos de ambiente y la musiquita del menu

---
+++ symphony-of-the-cryptids/escenas/menu_principal/transicion/intermedio_mapa_2.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+@onready var Fade = $AnimationPlayer
+
+func _ready():
+	Fade.play("Fades")
+	await Fade.animation_finished
+	get_tree().change_scene_to_file("res://escenas/nivel/escenario_3.tscn")
+	pass # Replace with function body.
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("SaltarMapa"):
+		get_tree().change_scene_to_file("res://escenas/nivel/escenario_3.tscn")
